6be567f86575cf7cd5fd17f94ef43accc7cd897f
KapkanGG <muhosransk300@gmail.com>
2025-04-28 19:56:22 +0300

    Написан js, добавлена ссылка в README.MDCD

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list')
 
-// @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+	const cardImage = cardElement.querySelector('.card__image')
+	const cardTitle = cardElement.querySelector('.card__title')
+	const deleteButton = cardElement.querySelector('.card__delete-button')
+	cardImage.src = cardData.link
+	cardImage.alt = cardData.name
+	cardTitle.textContent = cardData.name
+	deleteButton.addEventListener('click', () => {
+		deleteCard(cardElement)
+	})
 
-// @todo: Функция удаления карточки
+	return cardElement
+}
+function deleteCard(cardElement) {
+	cardElement.remove()
+}
+function renderCards() {
+	initialCards.forEach(cardData => {
+		const cardElement = createCard(cardData, deleteCard)
+		placesList.append(cardElement)
+	})
+}
 
-// @todo: Вывести карточки на страницу
+renderCards()
